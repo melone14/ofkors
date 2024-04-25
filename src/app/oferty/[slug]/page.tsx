eb34ca6d4ced57bf201cf6b05f548eccdf9b693f
SpexoCeo <patryk.wojciechowski.spexo@gmail.com>
2024-04-25 12:15:24 +0200

    updates

--- src/app/oferty/[slug]/page.tsx
+++ src/app/oferty/[slug]/page.tsx
@@ -13,9 +13,8 @@ import { ScrollComponent } from "@/components/scrollComponent/ScrollComponent";
 
 import CropIcon from '@mui/icons-material/Crop';
 import MeetingRoomIcon from '@mui/icons-material/MeetingRoom';
-import BathroomIcon from '@mui/icons-material/Bathroom';
+import CropFreeIcon from '@mui/icons-material/CropFree';
 import StairsIcon from '@mui/icons-material/Stairs';
-import PaidIcon from '@mui/icons-material/Paid';
 
 const RowComponent = ({ label, data }: { label:string, data:string | number | undefined}) =>{
 
@@ -65,7 +64,7 @@ export default async function Page({ params }: { params: { slug: string } }) {
                                 {(response.totalArea || response.noOfRooms || (response.totalArea || response.lotArea) || response.floorNo) && <div className="w-full flex flex-row flex-wrap items-center justify-center mb-14 gap-5 md:gap-x-16">
                                     {response.totalArea ?
                                         <div className="flex flex-col gap-y-2.5 items-center justify-center w-32 h-32">
-                                            <CropIcon sx={{ color:"#47141e", fontSize:'3rem'}}/>
+                                            <CropFreeIcon sx={{ color:"#47141e", fontSize:'3rem'}}/>
                                             <div>Powierzchnia</div>
                                             <div className="text-lg font-semibold">{response.totalArea}m<sup>2</sup></div>
                                         </div>
@@ -78,7 +77,7 @@ export default async function Page({ params }: { params: { slug: string } }) {
                                         </div> : null}
                                     {(response.totalArea || response.lotArea) ?
                                          <div className="flex flex-col gap-y-2.5 items-center justify-center w-32 h-32">
-                                            <PaidIcon sx={{ color:"#47141e", fontSize:'3rem'}}/>
+                                            <CropIcon sx={{ color:"#47141e", fontSize:'3rem'}}/>
                                             <div>Cena za m<sup>2</sup></div>
                                             <div className="text-lg font-semibold">{Math.round(parseFloat(response.price.amount)/((response.totalArea ? response.totalArea : response.lotArea) || 0))} {response.price.currency}</div>
                                         </div> 
